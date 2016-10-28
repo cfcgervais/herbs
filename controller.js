773ed062c2d8d0089d7a535aa06403f69e1030bd
Tyler Gervais <cfcgervais@gmail.com>
2016-10-27 21:35:17 -0600

    done?

--- controller.js
+++ controller.js
@@ -3,87 +3,159 @@ var snapSeed = 5300;
 var snap = 3000;
 var ranarr = 257;
 var ranSeed = 5295;
-var ranObj = [];
-var ranSeedObj = {};
-var snapObj = {};
-var snapSeedObj = {};
-herbController();
-console.log(ranObj)
+var ranObj;
+var ranSeedObj;
+var snapObj;
+var snapSeedObj;
 
+Promise.all([
+    herbService.getData(snap, setSnap),
+    herbService.getData(snapSeed, setSnapSeed),
+    herbService.getData(ranarr, setRan),
+    herbService.getData(ranSeed, setRanSeed)
+]).then(ready)
 
+function ready(data) {
+    setSnap(data[0])
+    setSnapSeed(data[1])
+    setRan(data[2])
+    setRanSeed(data[3])
+    ranObj = ranObj.split(',').join('');
+    ranSeedObj = ranSeedObj.split('k').join('00').split('.').join('');
+    snapObj = snapObj.split(',').join('');
+    snapSeedObj = snapSeedObj.split('k').join('00').split('.').join('');
+    parseInt(ranObj, 10)
+    parseInt(snapObj, 10)
+    parseInt(ranSeedObj, 10)
+    parseInt(snapSeedObj, 10)
+    var ranTotal = ranObj * 43;
+    var snapTotal = snapObj * 43;
+    var ranSeedTotal = ranSeedObj * 6;
+    var snapSeedTotal = snapSeedObj * 6;
+    var snapProf = snapTotal - snapSeedTotal;
+    var ranProf = ranTotal - ranSeedTotal;
 
-function herbController(){
-    herbService.getObject(snapSeed, setSnapSeed);
-    herbService.getObject(snap, setSnap);
-    herbService.getObject(ranSeed, setRanSeed);
-    herbService.getObject(ranarr, setRan);
+    function k(stuff){
+        return stuff > 999 ? (stuff/1000).toFixed(1) + 'k' : stuff
+    }
+    snapProf = k(snapProf);
+    ranProf = k(ranProf);
     
-    function setRan(data) {
-        var parsedData = JSON.parse(data);
-        ranObj.push(parsedData.item.current.price);
-    } 
-        // var rPrice = ranObj["Ranarr weed"];
-        
-    function setRanSeed(data){
-        var data = JSON.parse(data);
-        ranSeedObj[data.item.name] = data.item.current.price;
-        var rsPrice = ranSeedObj["Ranarr seed"]; 
-    }
-        
-    function setSnap(data){
-        var data = JSON.parse(data);
-        snapObj[data.item.name] = data.item.current.price;
-        var sPrice = snapObj["Snapdragon"]
-    }
-        
-    function setSnapSeed(data){
-        var data = JSON.parse(data);
-        snapSeedObj[data.item.name] = data.item.current.price;
-        var ssPrice = snapSeedObj["Snapdragon seed"]
-    }
-    
-    drawPrices();
-        
-    function drawPrices(){
-        var profitElem = $('#profits')
-        var template = `<div class="col-xs-6">
-                            <h1></h1>
-                            <h3></h3>
+    cardElem = $('#profits');
+    var template = '';
+    template = `<div class="row">
+                    <div class="col-xs-6">
+                        <div class="card">
+                            <h1>Snapdragons</h1>
+                            <h3>${snapProf} profit/run</h3>
                         </div>
-                        <div class="col-xs-6">
-                            <h1></h1>
-                            <h3></h3>
-                        </div>`
-        profitElem.append(template)
-    }
+                    </div>
+                    <div class="col-xs-6">
+                        <div class="card">
+                            <h1>Ranarrs</h1>
+                            <h3>${ranProf} profit/run</h3>
+                        </div>
+                    </div>
+                </div>`
+    cardElem.append(template)              
+}
+
+function setRan(data) {
+    var parsedData = JSON.parse(data);
+    var ranData = parsedData.item.current.price;
+    ranObj = ranData;
+}
+
+function setRanSeed(data) {
+    var parsedData = JSON.parse(data);
+    var ranSeedData = parsedData.item.current.price;
+    ranSeedObj = ranSeedData
+}
+
+function setSnap(data) {
+    var parsedData = JSON.parse(data);
+    var snapData = parsedData.item.current.price;
+    snapObj = snapData
+}
+
+function setSnapSeed(data) {
+    var parsedData = JSON.parse(data);
+    var snapSeedData = parsedData.item.current.price;
+    snapSeedObj = snapSeedData
 }
 
 
-   
+    
 
-        
-        
-        
     
 
 
 
-   
+ 
 
 
-        
-
-        
 
 
     
-        
-        
-      
     
-        
     
-        
-    
-        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- service.js
+++ service.js
@@ -1,13 +1,20 @@
 function HerbService(){
  var baseUrl = "http://services.runescape.com/m=itemdb_oldschool/api/catalogue/detail.json?item=";
 
-
-    this.getObject = function(id, cb){
-        $.get(baseUrl + id, cb);
-
+    this.getData = function(id, cb){
+      return new Promise(function(resolve, reject) {
+          $.get(baseUrl + id).then(resolve).catch(reject);
+      })
     }
-    
 }
+        
+
+
+        
+
+
+
+    
 
 
  
